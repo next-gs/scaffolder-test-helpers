b9c1907344e66fd6d0eb565b2adf8d3ead52dec4
Michael Barton <mail@michaelbarton.me.uk>
2011-01-04 10:38:31 -0500

    Update helper methods

--- lib/scaffolder/test/helpers.rb
+++ lib/scaffolder/test/helpers.rb
@@ -5,31 +5,32 @@ require 'scaffolder'
 module Scaffolder::Test
   module Helpers
 
-    def scaffold_and_sequence(entries)
-      scaffold = write_scaffold_file(entries.map do |entry|
-        {'sequence' => {'source' => entry['name']}}
-      end)
-      sequence = write_sequence_file(entries.map do |entry|
-        {:name => entry['name'], :sequence => entry['nucleotides']}
-      end)
-      [scaffold,sequence]
-    end
-
-    def write_sequence_file(*sequences)
-      file = Tempfile.new("sequence").path
+    def write_sequence_file(entries,file = Tempfile.new("sequence").path)
       File.open(file,'w') do |tmp|
-        sequences.flatten.each do |sequence|
-          seq = Bio::Sequence.new(sequence[:sequence])
-          tmp.print(seq.output(:fasta,:header => sequence[:name]))
+        make_sequence(entries).each do |entry|
+          seq = Bio::Sequence.new(entry[:sequence])
+          tmp.print(seq.output(:fasta,:header => entry[:name]))
         end
       end
       file
     end
 
-    def write_scaffold_file(scaffold)
-      file = Tempfile.new("scaffold").path
-      File.open(file,'w'){|tmp| tmp.print(YAML.dump(scaffold))}
+    def write_scaffold_file(entries,file = Tempfile.new("scaffold").path)
+      File.open(file,'w'){|tmp| tmp.print(YAML.dump(make_scaffold(entries)))}
       file
+    end
+
+    def make_scaffold(entries)
+      entries.map do |entry|
+        {'sequence' => {'source' => (entry['name'] || entry[:name]) }}
+      end
+    end
+
+    def make_sequence(entries)
+      entries.map do |entry|
+        {:name => (entry['name'] || entry[:name]),
+          :sequence => (entry['nucleotides'] || entry[:nucleotides]) }
+      end.flatten
     end
 
   end
