d413e504dee71aa1a05809273805ffe0350c7c24
Michael Barton <mail@michaelbarton.me.uk>
2011-06-07 16:04:27 -0400

    Refactor helper specs

--- spec/scaffolder/test/helpers_spec.rb
+++ spec/scaffolder/test/helpers_spec.rb
@@ -2,70 +2,75 @@ require File.join(File.dirname(__FILE__),'..','..','spec_helper')
 
 describe Scaffolder::Test::Helpers, "#generate_scaffold_files method" do
 
-  describe "pass an array with a single sequence" do
+  describe "#generate_scaffold_files" do
 
-    before(:all) do
-      entries = [Sequence.new(:name => 'c1',:sequence => 'ATG')]
-      @scaf_file, @seq_file = described_class.generate_scaffold_files(entries)
+    context "passed an array with a single sequence" do
+
+      before(:all) do
+        entries = [Sequence.new(:name => 'c1',:sequence => 'ATG')]
+        @scaf_file, @seq_file = described_class.generate_scaffold_files(entries)
+      end
+
+      it "should create the corresponding scaffold file" do
+        File.exists?(@scaf_file.path).should be_true
+        scaffold = YAML.load(File.read(@scaf_file.path))
+        scaffold.should == [{'sequence' => {'source' => 'c1'}}]
+      end
+
+      it "should create the corresponding sequence file" do
+        File.exists?(@seq_file.path).should be_true
+        sequence = File.read(@seq_file.path)
+        sequence.should == ">c1\nATG\n"
+      end
+
     end
 
-    it "should create the corresponding scaffold file" do
-      File.exists?(@scaf_file.path).should be_true
-      scaffold = YAML.load(File.read(@scaf_file.path))
-      scaffold.should == [{'sequence' => {'source' => 'c1'}}]
+    context "passed an array with two sequences" do
+
+      before(:all) do
+        entries = [Sequence.new(:name => 'c1',:sequence => 'ATG'),
+                   Sequence.new(:name => 'c2',:sequence => 'ATG')]
+        @scaf_file, @seq_file = described_class.generate_scaffold_files(entries)
+      end
+
+      it "should create the corresponding scaffold file" do
+        File.exists?(@scaf_file.path).should be_true
+        scaffold = YAML.load(File.read(@scaf_file.path))
+        scaffold.should == [{'sequence' => {'source' => 'c1'}},
+          {'sequence' => {'source' => 'c2'}}]
+      end
+
+      it "should create the corresponding sequence file" do
+        File.exists?(@seq_file.path).should be_true
+        sequence = File.read(@seq_file.path)
+        sequence.should == ">c1\nATG\n>c2\nATG\n"
+      end
+
     end
 
-    it "should create the corresponding sequence file" do
-      File.exists?(@seq_file.path).should be_true
-      sequence = File.read(@seq_file.path)
-      sequence.should == ">c1\nATG\n"
+    context "passed an array with a sequence and an unresolved region" do
+
+      before(:all) do
+        entries = [Sequence.new(:name => 'c1',:sequence => 'ATG'),
+                   Unresolved.new(:length => 5)]
+        @scaf_file, @seq_file = generate_scaffold_files(entries)
+      end
+
+      it "should create the corresponding scaffold file" do
+        File.exists?(@scaf_file.path).should be_true
+        scaffold = YAML.load(File.read(@scaf_file.path))
+        scaffold.should == [{'sequence' => {'source' => 'c1'}},
+          {'unresolved' => {'length' => 5}}]
+      end
+
+      it "should create the corresponding sequence file" do
+        File.exists?(@seq_file.path).should be_true
+        sequence = File.read(@seq_file.path)
+        sequence.should == ">c1\nATG\n"
+      end
+
     end
 
   end
 
-  describe "pass an array with two sequences" do
-
-    before(:all) do
-      entries = [Sequence.new(:name => 'c1',:sequence => 'ATG'),
-                 Sequence.new(:name => 'c2',:sequence => 'ATG')]
-      @scaf_file, @seq_file = described_class.generate_scaffold_files(entries)
-    end
-
-    it "should create the corresponding scaffold file" do
-      File.exists?(@scaf_file.path).should be_true
-      scaffold = YAML.load(File.read(@scaf_file.path))
-      scaffold.should == [{'sequence' => {'source' => 'c1'}},
-        {'sequence' => {'source' => 'c2'}}]
-    end
-
-    it "should create the corresponding sequence file" do
-      File.exists?(@seq_file.path).should be_true
-      sequence = File.read(@seq_file.path)
-      sequence.should == ">c1\nATG\n>c2\nATG\n"
-    end
-
-  end
-
-  describe "pass an array with a sequence and an unresolved region" do
-
-    before(:all) do
-      entries = [Sequence.new(:name => 'c1',:sequence => 'ATG'),
-                 Unresolved.new(:length => 5)]
-      @scaf_file, @seq_file = generate_scaffold_files(entries)
-    end
-
-    it "should create the corresponding scaffold file" do
-      File.exists?(@scaf_file.path).should be_true
-      scaffold = YAML.load(File.read(@scaf_file.path))
-      scaffold.should == [{'sequence' => {'source' => 'c1'}},
-        {'unresolved' => {'length' => 5}}]
-    end
-
-    it "should create the corresponding sequence file" do
-      File.exists?(@seq_file.path).should be_true
-      sequence = File.read(@seq_file.path)
-      sequence.should == ">c1\nATG\n"
-    end
-
-  end
 end
